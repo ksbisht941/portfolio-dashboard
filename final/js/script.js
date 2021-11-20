e9849fb9af827f5e17259100f35162ad2c7a4f73
ksbisht <ksbisht941@gmail.com>
2021-11-20 16:21:15 +0530

    portfolio dashboard

--- final/js/script.js
+++ final/js/script.js
@@ -11,7 +11,7 @@ VanillaTilt.init(document.querySelectorAll(".col .project-card"), {
 VanillaTilt.init(document.querySelectorAll(".chat-list .chat-item"), {
   max: 30,
   speed: 600,
-  scale: 1.02,
+  scale: 1,
   transition: true,
   easing: "cubic-bezier(.03,.98,.52,.99)",
   perspective: 600,
