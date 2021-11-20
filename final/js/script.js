3594c499721ebf0bfe80e441951cca89f34fb060
ksbisht <ksbisht941@gmail.com>
2021-11-20 16:17:48 +0530

    portfolio dashboard

---
+++ final/js/script.js
@@ -0,0 +1,19 @@
+VanillaTilt.init(document.querySelectorAll(".col .project-card"), {
+  max: 30,
+  speed: 600,
+  scale: 1,
+  transition: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  perspective: 600,
+  glare: true,
+});
+
+VanillaTilt.init(document.querySelectorAll(".chat-list .chat-item"), {
+  max: 30,
+  speed: 600,
+  scale: 1.02,
+  transition: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  perspective: 600,
+  glare: true,
+});
